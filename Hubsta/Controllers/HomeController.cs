8d35ddd26e52c6ab98928dba467b519ad8cc7415
KarlBro-Man <karlhenrid.manansala@gmail.com>
2024-11-10 13:48:50 +0800

    Added functionality in Register page

--- Hubsta/Controllers/HomeController.cs
+++ Hubsta/Controllers/HomeController.cs
@@ -12,12 +12,16 @@ namespace Hubsta.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext _context;
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, UserManager<AppUser> userManager)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, RoleManager<IdentityRole> roleManager)
         {
             _logger = logger;
             _context = context;
             _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
         }
 
         public IActionResult Index()
@@ -37,7 +41,41 @@ namespace Hubsta.Controllers
         [HttpPost]
         public async Task<IActionResult> Register(RegisterVM model)
         {
-            return View();
+            var user = await _userManager.FindByEmailAsync(model.Email);
+
+            if (user == null)
+            {
+                var newUser = new AppUser
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Email = model.Email,
+                    UserName = model.FirstName + " " + model.LastName,
+                    Gender = model.Gender
+                };
+                await _userManager.CreateAsync(newUser, model.Password!);
+                await _userManager.AddToRoleAsync(newUser, "user");
+                await _signInManager.SignInAsync(newUser, isPersistent: false);
+
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Hubsta/ViewModels/LoginVM.cs
@@ -0,0 +1,9 @@
+﻿namespace Hubsta.ViewModels
+{
+    public class LoginVM
+    {
+        public string? Email {  get; set; }
+        public string? Password { get; set; }
+        public bool IsPersistent {  get; set; }
+    }
+}
